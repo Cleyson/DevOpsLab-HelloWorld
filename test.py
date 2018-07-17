88eec2ce9b09e182cc776ca49e1e965b488a051a
cleyson.bonfim <cleyson.bonfim@gmail.com>
2018-07-17 07:50:21 -0400

    Update Travis File

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "DevOps Essentials CBM.")
+        self.assertRegex(result.data.decode(), "DevOps Essentials Cleyson Bonfim de Moura.")
 
 
 if __name__ == "__main__":
